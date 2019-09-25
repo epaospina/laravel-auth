4339c2df6f571e74653a34d9663deccd9741fa38
johandos <johan.ospina05@gmail.com>
2019-09-24 19:50:52 -0500

    se agrega al repositorio los modelos nuevos

---
+++ app/CarsPending.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CarsPending extends Model
+{
+    protected $table = 'cars_pending';
+}
---
+++ app/DriverData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DriverData extends Model
+{
+    protected $table = 'driver_data';
+}
