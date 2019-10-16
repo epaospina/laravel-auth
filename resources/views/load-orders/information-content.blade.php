7baefe13240a61a820581e316c74176875f74fc2
johandos <johan.ospina05@gmail.com>
2019-10-16 10:38:12 -0500

    se agrega color al boton de listar orden

--- resources/views/load-orders/information-content.blade.php
+++ resources/views/load-orders/information-content.blade.php
@@ -1,10 +1,16 @@
+<style>
+    .color-red{
+        color: red;
+        font-weight: 700;
+    }
+</style>
 @foreach($infoArray['information_car'] as $key => $infoCar)
     <div class="accordion" id="accordionExample">
         <div class="card">
             <div class="card-header" id="heading{{$key}}">
                 <h2 class="mb-0">
                     <button class="btn btn-light collapsed" type="button" data-toggle="collapse" data-target="#collapse{{$key}}" aria-expanded="false" aria-controls="collapse{{$key}}">
-                        Orden de carga coche #{{$key + 1}}
+                        Orden de carga coche con <span class="color-red">MODELO: </span>{{$infoCar['model_car']}} <span class="color-red">BASTIDOR: </span>{{$infoCar['vin']}}
                     </button>
                 </h2>
             </div>
